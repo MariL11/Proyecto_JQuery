86eb82d94d0d060d9de421f7f88f14f731f2f5a1
MariL11 <RMariL11777@gmail.com>
2024-03-02 13:20:01 +0100

    Añadir archivos
    Co-authored-by: SergioOrtigosa2000 <SergioOrtigosa2000@users.noreply.github.com>

--- jquery/jquery.js
+++ jquery/jquery.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+    
+
+    cargarPelicula ();
     inicializar();
 
 });
@@ -7,17 +10,94 @@ function inicializar(){
     $("img").css("filter","grayscale(100%)");
 }
 
+/*PREGUNTAS*/
+function cargarPelicula () {
 
 
-    $( "#primeraPeli").on( {   
+    $("#primeraPeli").on( {   
         mouseenter: function(){
-            $("#primeraPeli").css("filter","grayscale(0%)");
+            $("#primeraPeli").css("filter","brightness(150%)");
         },
 
         mouseleave: function(){
             $("#primeraPeli").css("filter","grayscale(100%)");
+        },
+        
+
+        click: function(){
+            $("#primeraPeli").css("border","10px solid blue");
+            $("#primeraPeli").css("filter","contrast(100%)");
+            $("#primeraPeli").css("filter","grayscale(0%)");
+            $("#caratula_ampliada").attr("src", "./img/pel_acc.png");
+           
+            $(".imagenes_img_img").each(function(index) {
+                $(this).attr("src", "./img/pel_acc_0" + (index+1) +".png");
+            })
+
+            $("figcaption").each(function(index) {
+                $(this).text("Acción " + (index+1));
+            })
         }
 
     });
+
+    $("#segundaPeli").on( {   
+        mouseenter: function(){
+            $("#segundaPeli").css("filter","brightness(150%)");
+        },
+
+        mouseleave: function(){
+            $("#segundaPeli").css("filter","grayscale(100%)");
+        },
+
+        click: function(){
+            $("#segundaPeli").css("border","10px solid blue");
+            $("#segundaPeli").css("filter","contrast(100%)");
+            $("#segundaPeli").css("filter","grayscale(0%)");
+            $("#caratula_ampliada").attr("src", "./img/pel_ani.png");
+
+            $(".imagenes_img_img").each(function(index) {
+                $(this).attr("src", "./img/pel_ani_0" + (index+1) +".png");
+            });
+
+            $("figcaption").each(function(index) {
+                $(this).text("Anime " + (index+1));
+            })
+           
+        }
+
+    });
+
+
+    $("#terceraPeli").on( {   
+        mouseenter: function(){
+            $("#terceraPeli").css("filter","brightness(150%)");
+        },
+
+        mouseleave: function(){
+            $("#terceraPeli").css("filter","grayscale(100%)");
+        },
+
+        click: function(){
+            $("#terceraPeli").css("border","10px solid blue");
+            $("#terceraPeli").css("filter","contrast(100%)");
+            $("#terceraPeli").css("filter","grayscale(0%)");
+            $("#caratula_ampliada").attr("src", "./img/pel_med.png");
+           
+            $(".imagenes_img_img").each(function(index) {
+                $(this).attr("src", "./img/pel_med_0" + (index+1) +".png");
+            })
+
+            $("figcaption").each(function(index) {
+                $(this).text("Historia " + (index+1));
+            })
+           
+        }
+
+    });
+
+
+};
+
 
 
