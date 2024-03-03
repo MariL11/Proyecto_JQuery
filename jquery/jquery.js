fc3c4d140ff240e932ae6b330be87b97289393cb
MariL11 <RMariL11777@gmail.com>
2024-03-03 13:00:13 +0100

    Añadir archivos
    Co-authored-by: SergioOrtigosa2000 <SergioOrtigosa2000@users.noreply.github.com>

--- jquery/jquery.js
+++ jquery/jquery.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     cargar_pelicula ();
     elegirImagen();
     inicializar();
-
+    //elegirPelicula();
 });
 
 function inicializar(){
@@ -15,6 +15,8 @@ function inicializar(){
 function cargar_pelicula () {
 
     let peliculaSeleccionada = null;
+
+    
     
 
     $("#primeraPeli, #segundaPeli, #terceraPeli").on({
@@ -43,14 +45,16 @@ function cargar_pelicula () {
 
             peliculaSeleccionada = $(this).css("filter", "contrast(150%)").css("border", "10px solid blue");
 
+            elegirPelicula(peliculaSeleccionada);
+
             $("#titulo").text($(this).attr("data-titulo"));
             $("#informacion").text($(this).attr("data-informacion"));
             $("#sinopsis").text($(this).attr("data-sinopsis"));
 
 
             if(peliculaSeleccionada.is("#primeraPeli")) {
-                console.log("Accion seleccionada");
                 $(".imagenes_img_img").each(function(index) {
+
                     $(this).attr("src", "./img/pel_acc_0" + (index+1) +".png");
                 });
             }else if(peliculaSeleccionada.is("#segundaPeli")){
@@ -65,10 +69,12 @@ function cargar_pelicula () {
 
                
             $("#primeraPeli").click(function(){
+
                 $("#caratula_ampliada").attr("src", "./img/pel_acc.png");
                 $("#video").attr("src", "./video/video_01.mp4");
-                $("#z_multimediad_img_img").attr("src", "./img/pel_acc_01.png");
-        
+                
+                //$("#z_multimediad_img_img").attr("src", "./img/pel_acc_01.png");
+
                 $("figcaption").each(function(index) {
                     $(this).text("Acción " + (index+1));
                 })
@@ -80,8 +86,9 @@ function cargar_pelicula () {
             $("#segundaPeli").click(function(){
                 $("#caratula_ampliada").attr("src", "./img/pel_ani.png");
                 $("#video").attr("src", "./video/video_02.mp4");
-                $("#z_multimediad_img_img").attr("src", "./img/pel_ani_01.png");
-               
+                //$("#z_multimediad_img_img").attr("src", "./img/pel_ani_01.png");
+                $("#z_multimediad_img_img").attr("src", ruta);
+
                 $("figcaption").each(function(index) {
                     $(this).text("Anime " + (index+1));
                 })
@@ -92,8 +99,9 @@ function cargar_pelicula () {
             $("#terceraPeli").click(function(){
                 $("#caratula_ampliada").attr("src", "./img/pel_med.png");
                 $("#video").attr("src", "./video/video_03.mp4");
-                $("#z_multimediad_img_img").attr("src", "./img/pel_med_01.png");
-        
+                //$("#z_multimediad_img_img").attr("src", "./img/pel_med_01.png");
+                $("#z_multimediad_img_img").attr("src", ruta);
+
                 $("figcaption").each(function(index) {
                     $(this).text("Historia " + (index+1));
                 })
@@ -108,23 +116,99 @@ function cargar_pelicula () {
 };
 
 
-    function elegirImagen(){
+function elegirImagen(){
+
+    //Preguntar
+    $("#primeraPeli").click(function(){
+        $(".imagenes_img_img").removeClass("imagenSeleccionada").css("filter", "grayscale(100%)").css("width","");
+        $("#z_multimediad_img_img").attr("src", "./img/pel_acc_01.png");
+        $(".imagenes_img_img").eq(0).addClass("imagenSeleccionada").css("filter", "brightness(150%)").css("width","80%");
+        $(".circulo_selector_img").removeClass("circulos");
+        $(".circulo_selector_img").eq(0).addClass("circulos");
+        //console.log(onclick($(".circulo_selector_img")));
+        elegirImagenNoPredeterminada();
+        elegirCirculo();
         
-        $(".imagenes_img_img").eq(2).on({
-
-            click: function() {
+    })
     
-                //console.log($(".imagenes_img_img").eq(3));
-                    
+    $("#segundaPeli").click(function(){
+        $(".imagenes_img_img").removeClass("imagenSeleccionada").css("filter", "grayscale(100%)").css("width","");
+        $("#z_multimediad_img_img").attr("src", "./img/pel_ani_01.png");
+        $(".imagenes_img_img").eq(0).addClass("imagenSeleccionada").css("filter", "brightness(150%)").css("width","80%");
+        $(".circulo_selector_img").removeClass("circulos");
+        $(".circulo_selector_img").eq(0).addClass("circulos");
+        //console.log(onclick($(".circulo_selector_img")));
+        elegirImagenNoPredeterminada();
+        elegirCirculo();
+    })
+
+    $("#terceraPeli").click(function(){
+        $(".imagenes_img_img").removeClass("imagenSeleccionada").css("filter", "grayscale(100%)").css("width","");
+        $("#z_multimediad_img_img").attr("src", "./img/pel_med_01.png");
+        $(".imagenes_img_img").eq(0).addClass("imagenSeleccionada").css("filter", "brightness(150%)").css("width","80%");
+        $(".circulo_selector_img").removeClass("circulos");
+        $(".circulo_selector_img").eq(0).addClass("circulos");
+        //console.log(onclick($(".circulo_selector_img")));
+        elegirImagenNoPredeterminada();  
+        elegirCirculo(); 
+    })
     
-                $(".imagenes_img_img").eq(2).addClass("imagenSeleccionada");
-
-            }    
-
-
-    })  
- 
 }
+
+function elegirImagenNoPredeterminada () {
+    $(".imagenes_img_img").on({
+
+        click: function() {
+
+            $(".imagenes_img_img").removeClass("imagenSeleccionada");
+
+            //Preguntar
+            let a = $(this).addClass("imagenSeleccionada").css("filter", "brightness(150%)").css("width","80%");
+
+        
+            
+
+            elegirCirculo(a);
+
+            $(".imagenes_img_img").not(".imagenSeleccionada").css("filter", "grayscale(100%)").css("width","");
+
+            let ruta = $(this).attr("src");
+
+            $("#z_multimediad_img_img").attr("src", ruta);
+        }    
+
+
+    }) 
+}
+
+function elegirCirculo (index) {
+    $(".circulo_selector_img").on({
+        click: function() {            
+            $(".circulo_selector_img").removeClass("circulos");
+            $(".circulo_selector_img").eq(index).addClass("circulos");
+            $(this).addClass("circulos");
+            
+        }
+    })
+}
+
+function elegirPelicula(peliculaSeleccionada){
+    
+
+        $('select').change(function() {
+            if($(this).val()=="peli1") {
+                peliculaSeleccionada = $("#primeraPeli");
+            } else if($(this).val()=="peli2") {
+                alert ("Se ha elegido la segunda pelicula.");
+            } else if($(this).val()=="peli13") {
+                alert ("Se ha elegido la tercera pelicula.");
+            }
+        });
+
+}
+
+
+
 
 
     /*click: function(){
